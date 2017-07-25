f47e8891a598ca3eaa5b5e4fbe3004808aa22e45
Your Name <you@example.com>
2017-07-25 17:57:43 +0000

    3

---
+++ web/ask/qa/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^question/([0-9]+)/$', views.test, name='test'),
+    url(r'^login/|singup/|ask/|popular/|new/$', views.test),
+    url(r'^$', view.test),
+]
