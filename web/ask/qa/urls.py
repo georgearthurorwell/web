0ed0f9a0ad5e999081c5c3a0c2698c99a11db11b
Your Name <you@example.com>
2017-07-25 18:50:50 +0000

    3.1

--- web/ask/qa/urls.py
+++ web/ask/qa/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^question/([0-9]+)/$', views.test, name='test'),
     url(r'^login/|singup/|ask/|popular/|new/$', views.test),
-    url(r'^$', view.test),
+    url(r'^$', views.test),
 ]
